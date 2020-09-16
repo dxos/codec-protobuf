da8e5319b45e93a8f744bc4d51f49b3fce6e722d
Dmytro Maretskyi <maretskii@gmail.com>
2020-09-16 11:53:28 +0200

    Add sbapshot tests

--- packages/codec-experimental-compiler/test/serializer.test.ts
+++ packages/codec-experimental-compiler/test/serializer.test.ts
@@ -1,7 +1,8 @@
 import { serializer } from './gen/serializer';
 import { MyKey } from './my-key';
+import { readFileSync } from 'fs'
 
-test('codec', async () => {
+test('encode and decode', async () => {
   const codec = serializer.getCodecForType('TaskList')
 
   const initial = {
@@ -26,4 +27,8 @@ test('codec', async () => {
   const decoded = codec.decode(encoded)
 
   expect(decoded).toEqual(initial)
+})
+
+test('definitions', () => {
+  expect(readFileSync(require.resolve('./gen/serializer'), { encoding: 'utf-8' })).toMatchSnapshot()
 })
