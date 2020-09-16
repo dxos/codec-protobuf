476876d2321616139c46578e3353264c1523da5f
Dmytro Maretskyi <maretskii@gmail.com>
2020-09-16 11:56:24 +0200

    Add primitive types

--- packages/codec-experimental-compiler/src/declaration-generator.ts
+++ packages/codec-experimental-compiler/src/declaration-generator.ts
@@ -14,7 +14,21 @@ function getFieldType(field: protobufjs.Field, subs: SubstitutionsMap): ts.TypeN
 
 function getScalarType(field: protobufjs.Field, subs: SubstitutionsMap): ts.TypeNode {
   switch(field.type) {
+    case 'double': return f.createKeywordTypeNode(ts.SyntaxKind.NumberKeyword);
+    case 'float': return f.createKeywordTypeNode(ts.SyntaxKind.NumberKeyword);
+    case 'int32': return f.createKeywordTypeNode(ts.SyntaxKind.NumberKeyword);
+    case 'int64': return f.createKeywordTypeNode(ts.SyntaxKind.NumberKeyword);
+    case 'uint32': return f.createKeywordTypeNode(ts.SyntaxKind.NumberKeyword);
+    case 'uint64': return f.createKeywordTypeNode(ts.SyntaxKind.NumberKeyword);
+    case 'sint32': return f.createKeywordTypeNode(ts.SyntaxKind.NumberKeyword);
+    case 'sint64': return f.createKeywordTypeNode(ts.SyntaxKind.NumberKeyword);
+    case 'fixed32': return f.createKeywordTypeNode(ts.SyntaxKind.NumberKeyword);
+    case 'fixed64': return f.createKeywordTypeNode(ts.SyntaxKind.NumberKeyword);
+    case 'sfixed32': return f.createKeywordTypeNode(ts.SyntaxKind.NumberKeyword);
+    case 'sfixed64': return f.createKeywordTypeNode(ts.SyntaxKind.NumberKeyword);
+    case 'bool': return f.createKeywordTypeNode(ts.SyntaxKind.BooleanKeyword);
     case 'string': return f.createKeywordTypeNode(ts.SyntaxKind.StringKeyword);
+    case 'bytes': return f.createTypeReferenceNode('Uint8Array');
     default:
       if(subs[field.type]) {
         return f.createTypeReferenceNode(subs[field.type]!)
