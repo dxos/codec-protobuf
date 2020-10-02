6c72a88e43c2164171aa520de03a77edbba402a6
Dmytro Maretskyi <maretskii@gmail.com>
2020-10-02 17:19:18 +0200

    Add argument checks

--- packages/codec-experimental-runtime/src/codec.ts
+++ packages/codec-experimental-runtime/src/codec.ts
@@ -23,11 +23,13 @@ export class Schema<T> {
   }
 
   getCodecForType<K extends keyof T & string> (typeName: K): Codec<T[K]> {
+    if(typeof typeName !== 'string') throw new TypeError('Expected `typeName` argument to be a string');
     const type = this._typesRoot.lookupType(typeName);
     return new Codec(type, this._mapping, this);
   }
 
   tryGetCodecForType (typeName: string): Codec {
+    if(typeof typeName !== 'string') throw new TypeError('Expected `typeName` argument to be a string');
     const type = this._typesRoot.lookupType(typeName);
     return new Codec(type, this._mapping, this);
   }
